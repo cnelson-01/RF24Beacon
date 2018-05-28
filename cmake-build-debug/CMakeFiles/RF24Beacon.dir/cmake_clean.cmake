7965ae3842c56f13f592fb9d3d2851deb9c5b4d5
Chris <cnelson1200@gmail.com>
2018-05-28 08:11:29 -0700

    inital commit

---
+++ cmake-build-debug/CMakeFiles/RF24Beacon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RF24Beacon.dir/main.cpp.o"
+  "RF24Beacon.pdb"
+  "RF24Beacon"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RF24Beacon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
